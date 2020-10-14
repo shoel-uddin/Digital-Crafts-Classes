6ceea0f75f7129efede9df8300678c1c56bcedd3
Shoel Uddin <shoel1989@gmail.com>
2020-10-14 12:13:12 -0400

    day3

---
+++ programming101/user_input.py
@@ -0,0 +1,11 @@
+name = input("Name Please:\n") #\n New line
+
+subject = input ("Favorite subject\n")
+
+age = input("How old are you?\n")
+
+if int(age) >= 21: #Need to use int() since its a number
+    print("Grab a Beer!")
+
+print(f"You said your name is {name}")
+
