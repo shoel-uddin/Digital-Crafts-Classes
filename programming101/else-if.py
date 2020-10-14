a83b391b31ef8438c6dbd3f562aee776ce175529
Shoel Uddin <shoel1989@gmail.com>
2020-10-14 12:35:27 -0400

    day3

---
+++ programming101/else-if.py
@@ -0,0 +1,16 @@
+age = input("How old are you?\n")
+
+if int(age) >= 21:
+    print("You are old enough\n")
+
+else:
+    print("You are not old enough\n")
+
+age = int(input("How old are you?\n"))
+
+if age == 21:
+    print ("You may have a drink\n")
+elif age >= 21:
+    print("You are defineitly got this\n")
+else:
+    print("You are not old enough\n")
