f9be222e135f6a1dd35680432b8f7c59d574708f
Shoel Uddin <shoel1989@gmail.com>
2020-10-14 12:49:04 -0400

    elif

---
+++ programming101/exercises/else-if-exercise.py
@@ -0,0 +1,8 @@
+number = int(input("Pick a number between 1 and 10\n"))
+
+if number == 2:
+    print ("You got it\n")
+elif number > 2:
+    print ("That's too high\n")
+else:
+    print ("You are close\n")
