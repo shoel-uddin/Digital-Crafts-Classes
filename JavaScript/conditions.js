e3603d71aae705c81d9733ed9baa4789bae0bd70
Shoel Uddin <shoel1989@gmail.com>
2020-11-02 14:38:09 -0500

    JavaScript

---
+++ JavaScript/conditions.js
@@ -0,0 +1,37 @@
+let a= 10, b = 20, c = 30
+
+// if (a === 10){
+//     console.log('a is actually 10')
+// } else {
+//     console.log("is not true")
+// }
+
+// if (a < 10){
+//     console.log('a is less than 10')
+// } else if (a < 20){
+//     console.log("is less than 20")
+// } else {
+//     console.log ('a is 10')
+// }
+
+a == 10 ? 'yes' : "no"
+
+let result = a == 11 ? 'yes' : "guess again" 
+
+result 
+
+if (a>5) console.log ('yep its more than 5')
+
+switch (a) {
+    case 20:
+        console.log('a is 20')
+        break;
+    case 15:
+        console.log('a is 15')
+        break;
+    case 10:
+        console.log('a is 10')
+        break;
+    default:
+        console.log("I really don't know")
+}
