a092cdc213ca8f9df8c3d7955deb3a1c34d37d87
Shoel Uddin <shoeluddin@Shoels-MacBook-Pro.local>
2020-11-30 10:07:26 -0500

    node

--- node/exercises/9-write-file/cars-add.js
+++ node/exercises/9-write-file/cars-add.js
@@ -5,7 +5,7 @@ let newCar = [{name:"Car1",speed:90,year:2015},{name:"Car2",speed:90,year:2015}]
 
 const addCar = (newCar)=>{
     cars.push(newCar)
-    fs.writeFile("cars.json", JSON.stringify(newCar),(err)=>{
+    fs.writeFile("cars.json", JSON.stringify(cars),'utf8',(err)=>{
             if (err) throw err;
             console.log("New Car data saved")
         }
