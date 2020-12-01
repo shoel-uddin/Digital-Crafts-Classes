c0ef483cd3231be92a74c01dab2bc9747241e357
Shoel Uddin <shoeluddin@Shoels-MacBook-Pro.local>
2020-12-01 16:09:03 -0500

    js-reboot

--- js-reboot/array-exploration.js
+++ js-reboot/array-exploration.js
@@ -15,15 +15,39 @@ const names = [
 //     console.log(`${oneName}`)
 // }
 
-const hello = (whom) => console.log(`Hello ${whom}`)
-const bye = (whom) => console.log(`Bye ${whom}`)
+// const hello = (whom) => console.log(`Hello ${whom}`)
+// const bye = (whom) => console.log(`Bye ${whom}`)
 
 // names.forEach(hello) // loops through to say Hello Alice ... so on.
 // names.forEach(bye)
 
-const combo = (name) => {
-    hello(name)
-    bye(name)
-}
+// const combo = (name) => {
+//     hello(name)
+//     bye(name)
+// }
 
-names.forEach(combo)
+// names.forEach(combo)
+// names.forEach((n) =>{ //same as other forEach
+//     hello(n)
+//     bye(n)
+// })
+
+//Demo of .Filter
+const helloLongName = (n) =>{
+    if (n.length > 4){
+        hello(n)
+    }
+}
+// const isNameLong = (n) => { //same as next code
+//     return n.length > 4 // same as next few lines of code
+//     // if (n.length >4){
+//     //     return true
+//     // }else{
+//     //     return false
+//     // }
+// }
+const isNameLong = (n) => n.length > 4
+
+const longNames = names.filter(isNameLong)
+console.log(longNames)
+//Implement .Filter
