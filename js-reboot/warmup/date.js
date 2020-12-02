798210c4ab86b739abcc0591bf15f048226df99e
Shoel Uddin <shoeluddin@Shoels-MacBook-Pro.local>
2020-12-02 10:44:34 -0500

    warmup

---
+++ js-reboot/warmup/date.js
@@ -0,0 +1,11 @@
+const date = () => {
+    return new Date()
+}
+
+module.exports = date
+
+// const date = () => new Date()
+// module.exports = date
+
+// const date = () => "02 Dec 2020"
+// module.exports = date
---
+++ js-reboot/warmup/index.js
@@ -0,0 +1,3 @@
+const date = require ('./date')
+
+console.log(date())
