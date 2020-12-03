a28aa7f3dddb966e55a2c8db9e0c86f682358ef7
Shoel Uddin <shoeluddin@Shoels-MacBook-Pro.local>
2020-12-03 14:04:19 -0500

    hello-express

--- hello-express/index.js
+++ hello-express/index.js
@@ -1,3 +1,13 @@
-const http = require('http')
-const express = require('express')
+const http = require('http');
+const express = require('express');
 
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (req,res) =>{
+    res.send("Hey how's it")
+});
+
+server.listen(3000, 'localhost', ()=> {
+    console.log('Running on port http://localhost:3000')
+});
