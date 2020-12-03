da004b789978a3db60a53d634d2b10757a833467
Shoel Uddin <shoeluddin@Shoels-MacBook-Pro.local>
2020-12-03 16:17:20 -0500

    hello-express

--- hello-express/index.js
+++ hello-express/index.js
@@ -8,6 +8,23 @@ app.get('/', (req,res) =>{
     res.send("Hey how's it")
 });
 
+app.get('/cats',(req,res)=>{
+    res.send(`
+    <ul>
+        <li><a href="/cats/oakley">Oakley</a></li>
+        <li><a href="/cats/Marco">Marco</a></li>
+    </ul>
+    `)
+})
+
+app.get('/cats/:catName',(req,res)=>{
+    const catName = req.params.catName
+    res.send(`
+    <h1>This is the way, ${catName}!</h1>
+    <a href="/cats/">Back</a>
+    `)
+})
+
 server.listen(3000, 'localhost', ()=> {
     console.log('Running on port http://localhost:3000')
 });
